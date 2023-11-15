e97f9003c34c84b9d8c8d7b05f59185d92983689
Nicholas George <nicholas.george32@gmail.com>
2023-11-14 23:12:11 -0500

    Clean up event loop, handle SDL_QUIT

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <SDL2/SDL.h>
+#include "SDL2/SDL.h"
 
 int main(void) {
-  int init = SDL_Init(SDL_INIT_EVENTS|SDL_INIT_VIDEO);
+  int init = SDL_Init(SDL_INIT_EVENTS|SDL_INIT_VIDEO|SDL_INIT_TIMER);
   if (0!=init){
-    printf("Failed to init, exit code: %d. Exiting\n", init);
+    printf("Failed to start SDL, exit code: %d. Exiting\n", init);
     exit(EXIT_FAILURE);
   }else{
     printf("Initialized SDL2\n");
@@ -14,23 +14,37 @@ int main(void) {
   SDL_Event event;
   bool quit = false;
 
+  SDL_Window *window;
+  window = SDL_CreateWindow("Snake", 0,0,640,480, SDL_WINDOW_OPENGL);
   while(!quit){
-    SDL_CreateWindow("Snake", 0,0,640,480, SDL_WINDOW_OPENGL);
-    SDL_Delay(500);
+    SDL_Delay(250);
     while (SDL_PollEvent(&event)){
+
       switch (event.type) {
+
         case SDL_KEYDOWN:
-          printf("Key pressed\n");
-          switch (event.key.keysym.sym) {
-            case SDLK_q:
-              printf("Quit detected\n");
-              quit = true;
-            default:
-              break;
+          if (event.key.keysym.sym == SDLK_q) {
+            printf("Quit Key\n");
+            quit = true;
+            SDL_DestroyWindow(window);
+          } else {
+            printf("Unknown key pressed\n");
           }
           break;
+
+        case SDL_QUIT:
+          printf("Quit event\n");
+          quit = true;
+          SDL_DestroyWindow(window);
+          break;
+
+        default:
+          break;
+
       }
 
     }
   }
+  SDL_Quit();
+  exit(0);
 }
