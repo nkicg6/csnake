61ad3d8604ff1506b63d70a98550f9bfd5d259d4
Nicholas George <nicholas.george32@gmail.com>
2023-11-15 20:50:30 -0500

    draw rect

--- main.c
+++ main.c
@@ -14,8 +14,28 @@ int main(void) {
   SDL_Event event;
   bool quit = false;
 
-  SDL_Window *window;
-  window = SDL_CreateWindow("Snake", 0,0,640,480, SDL_WINDOW_OPENGL);
+  SDL_Window *window = SDL_CreateWindow("Snake", 0,0,640,480, SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE);
+
+  if(!window){
+    printf("Failed to create window, SDL Error: %s\n", SDL_GetError());
+    exit(EXIT_FAILURE);
+  }
+
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+  if(!renderer){
+    printf("Failed to create renderer, SDL Error: %s\n", SDL_GetError());
+    exit(EXIT_FAILURE);
+  }
+  SDL_SetRenderDrawColor(renderer, 255,25,255,255);
+  SDL_Rect rect;
+  rect.x = 25;
+  rect.y = 0;
+  rect.w = 150;
+  rect.h = 150;
+
+  SDL_RenderFillRect(renderer, &rect);
+  SDL_RenderDrawRect(renderer, &rect);
+  SDL_RenderPresent(renderer);
   while(!quit){
     SDL_Delay(250);
     while (SDL_PollEvent(&event)){
