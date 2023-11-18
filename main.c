7a25b963f4db1d0a6e7a2f6f9e059e91c44075e2
Nicholas George <nicholas.george32@gmail.com>
2023-11-18 13:59:45 -0500

    box movement, edge detection

--- main.c
+++ main.c
@@ -4,15 +4,21 @@
 #include "SDL2/SDL.h"
 
 
-void draw_snake(SDL_Renderer* rend, SDL_Rect* rect){
+void draw_snake(SDL_Renderer *rend, SDL_Rect *rect){
   SDL_SetRenderDrawColor(rend, 255,255,255,255);
   SDL_RenderClear(rend);
   SDL_SetRenderDrawColor(rend, 255,25,255,255);
   SDL_RenderFillRect(rend, rect);
   SDL_RenderDrawRect(rend, rect);
-  SDL_RenderPresent(rend);
 }
 
+void draw_game(SDL_Renderer *rend, SDL_Rect *rect, int r, int g, int b){
+  SDL_SetRenderDrawColor(rend, r, g, b, 255);
+  SDL_RenderDrawRect(rend, rect);
+}
+
+const int WIN_WIDTH = 640;
+const int WIN_HEIGHT = 480; 
 
 //TODO 
 //Create playable window size with grid (min game size), re-size
@@ -30,16 +36,17 @@ int main(void) {
   SDL_Event event;
   bool quit = false;
 
-  SDL_Window *window = SDL_CreateWindow("Snake", 0,0,640,480, SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE);
+  SDL_Window *window = SDL_CreateWindow("Snake", 0, 0, WIN_WIDTH, WIN_HEIGHT, SDL_WINDOW_RESIZABLE);
 
   if(!window){
     printf("Failed to create window, SDL Error: %s\n", SDL_GetError());
     exit(EXIT_FAILURE);
   }
 
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-  if(!renderer){
-    printf("Failed to create renderer, SDL Error: %s\n", SDL_GetError());
+  SDL_Renderer *win_renderer = SDL_CreateRenderer(window, -1, 0);
+
+  if(!win_renderer){
+    printf("Failed to create win_renderer, SDL Error: %s\n", SDL_GetError());
     exit(EXIT_FAILURE);
   }
 
@@ -54,8 +61,15 @@ int main(void) {
   head.w = 25;
   head.h = 25;
 
-  draw_snake(renderer, &head);
+  SDL_Rect game_rect;
+  game_rect.x = 25;
+  game_rect.y = 0;
+  game_rect.w = 600;
+  game_rect.h = 480;
 
+  draw_snake(win_renderer, &head);
+  draw_game(win_renderer, &game_rect, 200, 200, 200);
+  SDL_RenderPresent(win_renderer);
   while(!quit){
     SDL_Delay(50);
     while (SDL_PollEvent(&event)){
@@ -64,30 +78,57 @@ int main(void) {
 
         case SDL_KEYDOWN:
           switch (event.key.keysym.sym){
+            case SDLK_i:
+              printf("headx = %d, heady = %d\n", head.x, head.y);
+              printf("game_rect = %d, game_rect = %d\n", game_rect.x+game_rect.w, game_rect.y+game_rect.h);
+              break;
             case SDLK_q:
               printf("Quit Key\n");
               quit = true;
-              SDL_DestroyWindow(window);
               break;
             case SDLK_LEFT:
               printf("Left key\n");
+              if ((head.x - step_x) < game_rect.x){
+                printf("Exceeds bounds\n");
+                break;
+              }
               head.x -= step_x;
-              draw_snake(renderer, &head);
+              draw_snake(win_renderer, &head);
+              draw_game(win_renderer, &game_rect, 200, 200, 200);
+              SDL_RenderPresent(win_renderer);
               break;
             case SDLK_RIGHT:
-              printf("Right key\n");
+               printf("Right key\n");
+              if (((head.x+head.w) + step_x) > (game_rect.x+game_rect.w)){
+                printf("Exceeds bounds\n");
+                break;
+              }
               head.x += step_x;
-              draw_snake(renderer, &head);
+              draw_snake(win_renderer, &head);
+              draw_game(win_renderer, &game_rect, 200, 200, 200);
+              SDL_RenderPresent(win_renderer);
               break; 
             case SDLK_UP:
               printf("Up key\n");
-              head.y -= step_y;
-              draw_snake(renderer, &head);
+              if ((head.y - step_y) < game_rect.y) {
+                printf("Exceeds bounds\n");
+                break;
+              }
+             head.y -= step_y;
+              draw_snake(win_renderer, &head);
+              draw_game(win_renderer, &game_rect, 200, 200, 200);
+              SDL_RenderPresent(win_renderer);
               break;            
             case SDLK_DOWN:
               printf("Down key\n");
-              head.y += step_y;
-              draw_snake(renderer, &head);
+              if ((head.y + head.h + step_y) > game_rect.y + game_rect.h){
+                printf("Exceeds bounds\n");
+                break;
+              }
+               head.y += step_y;
+              draw_snake(win_renderer, &head);
+              draw_game(win_renderer, &game_rect, 200, 200, 200);
+              SDL_RenderPresent(win_renderer);
               break;            
             default:
               printf("Unknown key pressed\n");
@@ -98,7 +139,6 @@ int main(void) {
         case SDL_QUIT:
           printf("Quit event\n");
           quit = true;
-          SDL_DestroyWindow(window);
           break;
 
         default:
@@ -108,6 +148,8 @@ int main(void) {
 
     }
   }
+  SDL_DestroyRenderer(win_renderer);
+  SDL_DestroyWindow(window);
   SDL_Quit();
   exit(0);
 }
