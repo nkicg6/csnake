ef5a0dec814b700bd2e52dfb1236f49ded672913
Nicholas George <nicholas.george32@gmail.com>
2023-11-25 21:45:33 -0500

    start over simpler

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 cc `pkg-config sdl2 --libs --cflags` -Wall -pedantic -std=gnu11 main.c -o snake
-echo "build complete, running..."
 ./snake
--- main.c
+++ main.c
@@ -1,117 +1,55 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "SDL2/SDL.h"
 
-const int TARGET_TICK = 1000/30;
-const int MVMT_SPEED_MS = 50; // rate of movement update. Lower is faster
-const int WIN_WIDTH = 640;
-const int WIN_HEIGHT = 640; 
-const int GAME_WIDTH = 500;
-const int GAME_HEIGHT = 500;
-const int GAME_POS_X = 20;
-const int GAME_POS_Y = 20;
-const int STEP_SIZE = 10;
-
-const int GRID_N = ((GAME_WIDTH+GAME_POS_X)/STEP_SIZE)-1;
-
-typedef enum {
-  UP,
-  DOWN,
-  LEFT,
-  RIGHT,
-  STOPPED,
-  OK,
-  BOUNDARY,
-} SnakeMvmtDirection;
-
-typedef struct {
-  SDL_Rect *snake;
-  SDL_Window *window;
-  SDL_Renderer *win_renderer;
-  int Head_x;
-  int Head_y;
-  int Fruit_x;
-  int Fruit_y;
-  int Grid_n;
-  int Step_size;
-  SnakeMvmtDirection State;
-} Game;
+#define WINDOW_W 640
+#define WINDOW_H 640
+#define GAME_SIZE 600
+#define STEP_SIZE 10
+#define MVMT_SPEED 50
+#define REFRESH_RATE 1000/30
 
 
-void draw_game(SDL_Renderer *rend, SDL_Rect *rect, int r, int g, int b){
-  SDL_SetRenderDrawColor(rend, r, g, b, 255);
-  SDL_RenderDrawRect(rend, rect);
+struct Game {
+  int window_w; 
+  int window_h;
+  int game_w;
+  int game_h;
+  int step;
+  int mvmt_speed;
+  SDL_Window* window;
+  SDL_Renderer* win_ren;
+};
+
+void draw_game(struct Game *g){
+      SDL_SetRenderDrawColor(g->win_ren, 255,255,255,255);
+      SDL_RenderClear(g->win_ren);
+      //render code here.
+      SDL_RenderPresent(g->win_ren);
 }
 
-//TODO
-void new_fruit(SDL_Renderer *rend, SDL_Rect grid[][GRID_N], int max_x, int max_y) {
-  int x = rand() % (max_x - 1);
-  int y = rand() % (max_y - 1);
-  //ensure doesn't appear in the snake
-  SDL_Rect r = grid[x][y]; 
-  SDL_SetRenderDrawColor(rend, 255,0,255,255);
-  SDL_RenderFillRect(rend, &r);
-}
-
-int main(void) {
-  //Game game = make_game();
-
-  if (0!=SDL_Init(SDL_INIT_EVENTS|SDL_INIT_VIDEO|SDL_INIT_TIMER)){
-    printf("Failed to start SDL, error: %s\n", SDL_GetError());
-    exit(EXIT_FAILURE);
-  }else{
-    printf("Initialized SDL2\n");
-  }
-
-  SDL_Event event;
-
-  SDL_Window *window = SDL_CreateWindow("Snake", 0, 0, WIN_WIDTH, WIN_HEIGHT, SDL_WINDOW_RESIZABLE);
-
-  if(!window){
-    printf("Failed to create window, SDL Error: %s\n", SDL_GetError());
+int main(void){
+  if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER|SDL_INIT_EVENTS)!=0){
+    puts("Failed to init SDL");
     exit(EXIT_FAILURE);
   }
+  struct Game game = {.window_w=WINDOW_W, 
+    .window_h=WINDOW_H, 
+    .game_w=GAME_SIZE, 
+    .game_h=GAME_SIZE, 
+    .step=STEP_SIZE,
+    .mvmt_speed=MVMT_SPEED};
 
-  SDL_Renderer *win_renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC);
-
-  if(!win_renderer){
-    printf("Failed to create win_renderer, SDL Error: %s\n", SDL_GetError());
+  SDL_CreateWindowAndRenderer(WINDOW_W, WINDOW_H, SDL_WINDOW_RESIZABLE, &game.window, &game.win_ren);
+  if (!game.window || !game.win_ren) {
+    printf("Failed to create window or renderer. Error: %s", SDL_GetError());
     exit(EXIT_FAILURE);
   }
-
-  if(SDL_SetRenderDrawBlendMode(win_renderer, SDL_BLENDMODE_BLEND)!=0){
-    printf("Failed to set blend mode, SDL Error: %s\n", SDL_GetError());
-    exit(EXIT_FAILURE);
-  }
-  SDL_Rect game_rect = {.x = GAME_POS_X, .y = GAME_POS_Y, .w = GAME_WIDTH, .h = GAME_HEIGHT}; 
-
-  int head_start_pos_x = GAME_WIDTH/2;
-  int head_start_pos_y = GAME_HEIGHT/2;
-
-  // snake, fixed length for now.
-  int tailLen = 19;
-  SDL_Rect snake[tailLen];
-  SDL_Rect head = {.x = head_start_pos_x, .y = head_start_pos_y, .w = STEP_SIZE, .h = STEP_SIZE};
-  snake[0] = head;
-  for (int i = 1; i < tailLen; i++){
-    snake[i] = (SDL_Rect){.x = head_start_pos_x-STEP_SIZE*i, .y=head_start_pos_y, .w=STEP_SIZE,.h=STEP_SIZE};
-  }
-
-  SDL_Rect grid[GRID_N][GRID_N];
-
-  for (int i=0,x=GAME_POS_X; i < GRID_N-1; i++){
-    for (int j=0,y=GAME_POS_Y; j < GRID_N-1; j++){
-      grid[i][j] = (SDL_Rect){.x=x, .y=y, .w=STEP_SIZE, .h=STEP_SIZE};
-      y = y+STEP_SIZE;
-    }
-    x= x+STEP_SIZE;
-  }
-
-  SnakeMvmtDirection snake_direction = STOPPED;
-  SnakeMvmtDirection state = OK;
+  SDL_SetWindowTitle(game.window, "snake");
   bool quit = false;
-  int next_move_time = SDL_GetTicks() + MVMT_SPEED_MS;
+  SDL_Event event;
+  int next_move_time = SDL_GetTicks() + game.mvmt_speed;
 
   while(!quit){
     int last_tick = SDL_GetTicks();
@@ -121,151 +59,31 @@ int main(void) {
         quit = true;
         break;
       }
-
       if (event.type == SDL_KEYDOWN){
         switch (event.key.keysym.sym){
           case SDLK_i:
-            //debug info
-            printf("headx = %d, headx right: %d, heady = %d, heady max = %d\n", snake[0].x, snake[0].x + snake[0].w, snake[0].y, snake[0].y + snake[0].h);
+            puts("debug info here");
             break;
-
-          case SDLK_LEFT:
-            if (snake_direction == RIGHT || (snake_direction == STOPPED && state == BOUNDARY)) {
-              printf("Can't turn like that!\n");
-              break;
-            }
-            snake_direction = LEFT;
-            printf("left button\n");
-            break;
-
-          case SDLK_RIGHT:
-            if (snake_direction == LEFT || (snake_direction == STOPPED && state == BOUNDARY)) {
-              printf("Can't turn like that!\n");
-              break;
-            }
-            snake_direction = RIGHT;
-            printf("right button\n");
-            break;
-
-          case SDLK_UP:
-            if (snake_direction == DOWN || (snake_direction == STOPPED && state == BOUNDARY)) {
-              printf("Can't turn like that!\n");
-              break;
-            }
-            snake_direction = UP;
-            printf("up button\n");
-            break;
-
-          case SDLK_DOWN:
-            if (snake_direction == UP || (snake_direction == STOPPED && state == BOUNDARY)) {
-              printf("Can't turn like that!\n");
-              break;
-            }
-            snake_direction = DOWN;
-            printf("down button\n");
-            break;
-
-          case SDLK_SPACE:
-            snake_direction = STOPPED;
-            printf("pause\n");
-            break;
-
+            //arrow movements
           default:
             break;
         }
       }
     }
-    // update events
+
     if ( SDL_GetTicks() >= next_move_time) {
-      switch(snake_direction){
-        case UP:
-          if (snake[0].y-STEP_SIZE < game_rect.y) {
-            printf("Exceeds bounds\n");
-            snake_direction = STOPPED;
-            break;
-          }
-
-          for (int i=tailLen-1; i>0; i--){
-            snake[i] = snake[i-1];
-          }
-
-          snake[0].y -= STEP_SIZE;
-          break;
-
-        case DOWN:
-          if (snake[0].y+snake[0].h+STEP_SIZE > game_rect.y+game_rect.h){
-            snake_direction = STOPPED;
-            break;
-          }
-
-          for (int i=tailLen-1; i>0; i--){
-            snake[i] = snake[i-1];
-          }
-          snake[0].y += STEP_SIZE;
-          break;
-
-        case LEFT:
-          if (snake[0].x-STEP_SIZE < game_rect.x){
-            snake_direction = STOPPED;
-            break;
-          }
-
-          for (int i=tailLen-1; i>0; i--){
-            snake[i] = snake[i-1];
-          }
-
-          snake[0].x -= STEP_SIZE;
-          break;
-
-        case RIGHT:
-          if (snake[0].x+snake[0].w+STEP_SIZE> game_rect.x+game_rect.w){
-            printf("Exceeds bounds\n");
-            snake_direction = STOPPED;
-            state = BOUNDARY;
-            break;
-          }
-
-          for (int i=tailLen-1; i>0; i--){
-            snake[i] = snake[i-1];
-          }
-
-          snake[0].x += STEP_SIZE;
-          break;
-        case OK:
-          break;
-        case BOUNDARY:
-        case STOPPED:
-          break;
-      }
+      //update
       //render
+      draw_game(&game);
       int cur_delay = SDL_GetTicks() - last_tick;
-      if (cur_delay < TARGET_TICK) {
+      if (cur_delay < REFRESH_RATE) {
         SDL_Delay(cur_delay);
       }
-      //fn to draw based on game struct with all shapes
-      SDL_SetRenderDrawColor(win_renderer, 255,255,255,255);
-      SDL_RenderClear(win_renderer);
-      SDL_SetRenderDrawColor(win_renderer, 0,255,0,255);
-      SDL_RenderFillRect(win_renderer, &snake[0]);
-      SDL_SetRenderDrawColor(win_renderer, 255,25,255,100);
-      SDL_RenderFillRects(win_renderer, &snake[1], tailLen-1); 
-      SDL_SetRenderDrawColor(win_renderer, 200,200,200,100);
-      SDL_RenderDrawRects(win_renderer, *grid, GRID_N*GRID_N);
-      draw_game(win_renderer, &game_rect, 200, 200, 200);
-      if (state == BOUNDARY){
-        new_fruit(win_renderer, grid, GRID_N,GRID_N);
-      }
-      SDL_RenderPresent(win_renderer);
-      //Snake movement speed
-      next_move_time = SDL_GetTicks() + MVMT_SPEED_MS;
-      //uncomment for single step movemnet for debug
-      //snake_direction = STOPPED;
+
+      next_move_time = SDL_GetTicks() + game.mvmt_speed;
     }
   }
-
-  //cleanup
-  SDL_DestroyRenderer(win_renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+  SDL_DestroyRenderer(game.win_ren);
+  SDL_DestroyWindow(game.window);
   exit(0);
 }
